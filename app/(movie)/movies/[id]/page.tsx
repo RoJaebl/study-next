73cf7fe2b1833742ca8e38ecab08472c16c5f2bd
RoJaebl <qgs0924@gmail.com>
2024-07-20 20:06:14 +0900

    :hammer: #3.4 Parallel Requests

--- app/(movie)/movies/[id]/page.tsx
+++ app/(movie)/movies/[id]/page.tsx
@@ -1,7 +1,24 @@
-export default function MovieDetail({
+import { BASE_URL } from "../../../(home)/page";
+
+async function getMovie(id: string) {
+  console.log(`Fetcing movies: ${Date.now()}`);
+  await new Promise((res) => setTimeout(res, 2000));
+  const res = await fetch(`${BASE_URL}/${id}`);
+  return res.json();
+}
+
+async function getVideos(id: string) {
+  console.log(`Fetcing videos: ${Date.now()}`);
+  await new Promise((res) => setTimeout(res, 2000));
+  const res = await fetch(`${BASE_URL}/${id}/videos`);
+  return res.json();
+}
+
+export default async function MovieDetail({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  return <h1>Movie {id}</h1>;
+  const [movie, videos] = await Promise.all([getMovie(id), getVideos(id)]);
+  return <h1>{movie.title}</h1>;
 }
